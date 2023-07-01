87aaecf0c60bbdbbf6efdcb3503c37347e9b0906
jungheeeun <jungheeeun272@gmail.com>
2023-07-01 19:18:31 +0900

    ts build

---
+++ build/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import UserFirstart from './components/UserFirestart';
+import Video from './components/Video';
+
+function App() {
+  return (
+    <div>
+      <UserFirstart />
+      <Video />
+    </div>
+  );
+}
+
+export default App;
